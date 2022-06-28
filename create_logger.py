296a8f304295bb45bcec30edecc3c18b20d659e9
Bonsa11 <sambodza@hotmail.co.uk>
2022-06-28 15:37:10 +0100

    Fixed logging level elif list

--- create_logger.py
+++ create_logger.py
@@ -13,13 +13,13 @@ def create_logger(file: str, name: str, level: str):
     if level == 'DEBUG':
         logger.setLevel(logging.DEBUG)
     elif level == 'INFO':
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.INFO)
     elif level == 'WARNING':
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.WARNING)
     elif level == 'ERROR':
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.ERROR)
     elif level == 'CRITICAL':
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.CRITICAL)
 
     # create a file to store logs
     logfile = logging.FileHandler(file)
